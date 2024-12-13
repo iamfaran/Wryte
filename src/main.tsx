5e14b15278cdc6b10806b88ca355cf906bdc130c
Faran <faran1997@outlook.com>
2024-12-13 19:17:47 +0500

    wrapped the provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,25 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-import { worker } from './api/server'
+// import { worker } from './api/server'
 
 import './primitiveui.css'
 import './index.css'
+import { Provider } from 'react-redux'
+import {store} from './app/store'
 
 // Wrap app rendering so we can wait for the mock API to initialize
-async function start() {
+function start() {
   // Start our mock API server
-  await worker.start({ onUnhandledRequest: 'bypass' })
+  // await worker.start({ onUnhandledRequest: 'bypass' })
 
   const root = createRoot(document.getElementById('root')!)
 
   root.render(
-    <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>,
+    </Provider>
+    
   )
 }
 
