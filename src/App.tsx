04f2404ef19d4f8bfbf28234d1106ae8f6b7f8b2
Faran <faran1997@outlook.com>
2024-12-15 00:34:57 +0500

    show single post page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
-import { PostsList } from './features/posts/PostsList'
-import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsMainPage } from './features/posts/PostsMainPage'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
+  console.log("APp rendered")
   return (
     <Router>
       <Navbar />
       <div className="App">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <AddPostForm />
-                <PostsList />
-              </>
-            }
-          ></Route>
+          <Route path="/" element={<PostsMainPage />} />
+          <Route path="/posts/:postId" element={<SinglePostPage />} />
         </Routes>
       </div>
     </Router>
---
+++ src/features/posts/SinglePostPage.tsx
@@ -0,0 +1,30 @@
+import { useParams } from 'react-router-dom'
+
+import { useAppSelector } from '@/app/hooks'
+
+export const SinglePostPage = () => {
+  const { postId } = useParams()
+    console.log("rendering single post page")
+  const post = useAppSelector(state =>{
+    console.log(state)
+    return state.posts.find(post => post.id === postId)
+  }
+  )
+
+  if (!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <article className="post">
+        <h2>{post.title}</h2>
+        <p className="post-content">{post.content}</p>
+      </article>
+    </section>
+  )
+}
