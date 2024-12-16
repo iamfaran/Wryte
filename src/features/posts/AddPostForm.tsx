b0ff576bbabad0b381d9e85415205ca308216382
Faran <faran1997@outlook.com>
2024-12-16 16:34:40 +0500

    add user select in add post form

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { nanoid } from '@reduxjs/toolkit'
-import { postAdded} from './postsSlice'
-import { useAppDispatch } from '@/app/hooks'
+import { postAdded } from './postsSlice'
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import { selectAllUsers } from '../users/usersSlice'
 // TS types for the input fields
 // See: https://epicreact.dev/how-to-type-a-react-form-on-submit-handler/
 interface AddPostFormFields extends HTMLFormControlsCollection {
   postTitle: HTMLInputElement
   postContent: HTMLTextAreaElement
+  postAuthor: HTMLSelectElement
 }
 interface AddPostFormElements extends HTMLFormElement {
   readonly elements: AddPostFormFields
@@ -14,7 +16,8 @@ interface AddPostFormElements extends HTMLFormElement {
 
 export const AddPostForm = () => {
   const dispatch = useAppDispatch()
-  
+  const users = useAppSelector(selectAllUsers)
+
   const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
     // Prevent server submission
     e.preventDefault()
@@ -22,15 +25,20 @@ export const AddPostForm = () => {
     const { elements } = e.currentTarget
     const title = elements.postTitle.value
     const content = elements.postContent.value
+    const userId = elements.postAuthor.value
 
-    
-
-    dispatch(postAdded(title, content))
+    dispatch(postAdded(title, content, userId))
 
     console.log('Values: ', { title, content })
 
     e.currentTarget.reset()
   }
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
 
   return (
     <section>
@@ -38,13 +46,13 @@ export const AddPostForm = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="postTitle">Post Title:</label>
         <input type="text" id="postTitle" defaultValue="" required />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" name="postAuthor" required>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          defaultValue=""
-          required
-        />
+        <textarea id="postContent" name="postContent" defaultValue="" required />
         <button>Save Post</button>
       </form>
     </section>
--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -2,23 +2,18 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { nanoid } from '@reduxjs/toolkit'
 import type { RootState } from '@/app/store'
 
+type PostUpdate = Pick<Post, 'id' | 'title' | 'content'>
+
 export interface Post {
   id: string
   title: string
   content: string
+  user: string
 }
 
 const initialState: Post[] = [
-  {
-    id: '1',
-    title: 'First Post',
-    content: 'This is the first post',
-  },
-  {
-    id: '2',
-    title: 'Second Post',
-    content: 'This is the second post',
-  },
+  { id: '1', title: 'First Post!', content: 'Hello!', user: '0' },
+  { id: '2', title: 'Second Post', content: 'More text', user: '2' },
 ]
 
 export const postsSlice = createSlice({
@@ -29,13 +24,13 @@ export const postsSlice = createSlice({
       reducer(state, action: PayloadAction<Post>) {
         state.push(action.payload)
       },
-      prepare(title: string, content: string) {
+      prepare(title: string, content: string, userId: string) {
         return {
-          payload: { id: nanoid(), title, content },
+          payload: { id: nanoid(), title, content, user: userId },
         }
       },
     },
-    postUpdated(state, action: PayloadAction<Post>) {
+    postUpdated(state, action: PayloadAction<PostUpdate>) {
       const { id, title, content } = action.payload
       console.log(state)
       const existingPost = state.find((post) => post.id === id)
