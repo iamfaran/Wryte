ac3e6eca23856d6e008cffa3f9adbc8bcf46989b
Faran <faran1997@outlook.com>
2024-12-30 16:35:20 +0500

    add notifications feature

--- src/features/notifications/NotificationsList.tsx
+++ src/features/notifications/NotificationsList.tsx
@@ -4,14 +4,27 @@ import { TimeAgo } from '@/components/TimeAgo'
 
 import { PostAuthor } from '@/features/posts/PostAuthor'
 
-import { selectAllNotifications } from './notificationsSlice'
+import { selectAllNotifications, allNotificationsRead } from './notificationsSlice'
+import { useLayoutEffect } from 'react'
+import classnames from 'classnames'
+import { useAppDispatch } from '@/app/hooks'
 
 export const NotificationsList = () => {
+  const dispatch = useAppDispatch()
   const notifications = useAppSelector(selectAllNotifications)
 
+  useLayoutEffect(() => {
+    console.log(' useLayoutEffect')
+    dispatch(allNotificationsRead())
+  })
+
   const renderedNotifications = notifications.map((notification) => {
+    const notificationClassname = classnames('notification', {
+      new: notification.isNew,
+    })
+
     return (
-      <div key={notification.id} className="notification">
+      <div key={notification.id} className={notificationClassname}>
         <div>
           <b>
             <PostAuthor userId={notification.user} showPrefix={false} />
