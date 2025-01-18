98325f8c14696ced4e2f0b7f906188cf9e631e7b
Faran <faran1997@outlook.com>
2025-01-19 02:20:26 +0500

    drawer fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import Button from '@mui/material/Button'
-import Badge from '@mui/material/Badge'
-import MenuItem from '@mui/material/MenuItem'
-import Menu from '@mui/material/Menu'
+import React, { useState } from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+  Badge,
+  MenuItem,
+  Menu,
+  Drawer,
+  Box,
+  Divider,
+} from '@mui/material'
 import { Link } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { selectCurrentUser } from '@/features/users/usersSlice'
 import { logout } from '@/features/auth/authSlice'
 import { fetchNotifications, selectUnreadNotificationsCount } from '@/features/notifications/notificationsSlice'
 import { UserIcon } from './UserIcon'
-import { useState } from 'react'
-import MenuIcon from '@mui/icons-material/Menu'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import { Drawer } from '@mui/material'
-import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider'
-import { useParams } from 'react-router-dom'
 import { Layout } from './Layout'
+
+// Import icons
+import MenuIcon from '@mui/icons-material/Menu'
+import ArticleIcon from '@mui/icons-material/Article'
+import PeopleIcon from '@mui/icons-material/People'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import RefreshIcon from '@mui/icons-material/Refresh'
+import LogoutIcon from '@mui/icons-material/Logout'
 
 export const Navbar = () => {
   const smallScreen = useMediaQuery('(max-width: 768px)')
-
   const dispatch = useAppDispatch()
   const user = useAppSelector(selectCurrentUser)
   const numUnreadNotifications = useAppSelector(selectUnreadNotificationsCount)
-
   const isLoggedIn = !!user
-
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const [drawer, setDrawer] = useState<boolean>(false)
+
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
   }
@@ -50,6 +55,32 @@ export const Navbar = () => {
     dispatch(fetchNotifications())
   }
 
+  const NavButtons = () => (
+    <>
+      <Button color="inherit" component={Link} to="/posts" startIcon={<ArticleIcon />}>
+        Posts
+      </Button>
+      <Button color="inherit" component={Link} to="/users" startIcon={<PeopleIcon />}>
+        Users
+      </Button>
+      <Button
+        color="inherit"
+        component={Link}
+        to="/notifications"
+        startIcon={
+          <Badge badgeContent={numUnreadNotifications} color="error">
+            <NotificationsIcon />
+          </Badge>
+        }
+      >
+        Notifications
+      </Button>
+      <IconButton color="inherit" onClick={fetchNewNotifications} size="small">
+        <RefreshIcon />
+      </IconButton>
+    </>
+  )
+
   return (
     <AppBar position="sticky">
       <Layout>
@@ -61,22 +92,11 @@ export const Navbar = () => {
             smallScreen ? (
               <>
                 <Drawer anchor="left" open={drawer} onClose={() => setDrawer(false)}>
-                  <Box sx={{ padding: 2, textAlign: 'center', fontWeight: 'bold' }}>{user.name}</Box>
+                  <Box sx={{ p: 2, fontWeight: 'bold' }}>{user.name}</Box>
                   <Divider />
-                  <Button color="inherit" component={Link} to="/posts">
-                    Posts
-                  </Button>
-                  <Button color="inherit" component={Link} to="/users">
-                    Users
-                  </Button>
-                  <Button color="inherit" component={Link} to="/notifications">
-                    <Badge badgeContent={numUnreadNotifications} color="error">
-                      Notifications
-                    </Badge>
-                  </Button>
-                  <Button color="inherit" onClick={fetchNewNotifications}>
-                    Refresh Notifications
-                  </Button>
+                  <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 2 }}>
+                    <NavButtons />
+                  </Box>
                 </Drawer>
 
                 <IconButton size="large" edge="end" color="inherit" onClick={() => setDrawer(true)}>
@@ -85,31 +105,16 @@ export const Navbar = () => {
               </>
             ) : (
               <>
-                <Button color="inherit" component={Link} to="/posts">
-                  Posts
-                </Button>
-                <Button color="inherit" component={Link} to="/users">
-                  Users
-                </Button>
-                <Button color="inherit" component={Link} to="/notifications">
-                  <Badge badgeContent={numUnreadNotifications} color="error">
-                    Notifications
-                  </Badge>
-                </Button>
-                <Button color="inherit" onClick={fetchNewNotifications}>
-                  Refresh Notifications
-                </Button>
+                <NavButtons />
                 <IconButton size="large" edge="end" color="inherit" onClick={handleMenu}>
                   <UserIcon size={32} />
                 </IconButton>
-                {/* Menu takes 3 props  
-              anchorEL - the element that the menu is attached to
-              open - whether the menu is open or not
-              onClose - what to do when the menu is closed
-            */}
                 <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
                   <MenuItem disabled>{user.name}</MenuItem>
-                  <MenuItem onClick={onLogoutClicked}>Log Out</MenuItem>
+                  <MenuItem onClick={onLogoutClicked}>
+                    <LogoutIcon sx={{ mr: 1 }} />
+                    Log Out
+                  </MenuItem>
                 </Menu>
               </>
             )
